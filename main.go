feba0ff22a8de90ca625a3cf340fb5d334dc1170
Danny Rottstegge <danny@rottstegge.net>
2022-04-20 17:13:34 +0200

    refactor: move cli feature to separate go file

--- main.go
+++ main.go
@@ -1,84 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"gonitor/common"
-	"gonitor/monitors"
-	"os"
-	"strings"
-	"time"
 )
 
 func main() {
 	args := common.GetArgs()
-	mode := args.GetFirstRequired("m", "mode")
-	repeat := false
-	if args.HasAnyKey("r", "repeat") {
-		repeat = true
-	}
-	res := false
 
-	for {
-		switch strings.ToLower(mode) {
-		case "ping":
-			host := args.GetFirstRequired("H", "host")
-			m := monitors.PingMonitor{Host: host}
-			fmt.Printf("%v -> ", host)
-			ok, _ := m.Monitor()
-			res = ok
-			break
-		case "rest":
-			url := args.GetFirstRequired("u", "url")
-			m := monitors.RestMonitor{URL: url}
-			fmt.Printf("%v -> ", url)
-			ok, _ := m.Monitor()
-			res = ok
-			break
-		case "ssh":
-			host := args.GetFirstRequired("H", "host")
-			port := args.GetFirstDefault("22", "p", "port")
-			user := args.GetFirstRequired("u", "user")
-			pass := args.GetFirstRequired("P", "password", "pass")
-			m := monitors.SSHMonitor{
-				Host:     host,
-				Port:     port,
-				User:     user,
-				Password: pass,
-				Command:  "df -h /",
-				ResultParser: func(out string) bool {
-					return true
-				},
-			}
-			fmt.Printf("%v -> ", host)
-			ok, _ := m.Monitor()
-			res = ok
-			break
-		case "port":
-			host := args.GetFirstRequired("H", "host")
-			port := args.GetFirstRequired("p", "port")
-			m := monitors.PortMonitor{
-				Host: host,
-				Port: port,
-			}
-			fmt.Printf("%v:%v -> ", host, port)
-			ok, _ := m.Monitor()
-			res = ok
-			break
-		default:
-			panic("Unkown mode: " + mode)
-		}
-
-		fmt.Printf("%v\n", res)
-		if !repeat {
-			break
-		}
-		res = false
-		time.Sleep(5 * time.Second)
-	}
-
-	if res {
-		os.Exit(0)
-	} else {
-		os.Exit(1)
+	if args.HasAnyKey("m", "mode") {
+		common.Cli(args)
+		return
 	}
 }
